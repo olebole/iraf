600a27c8964165c1ff1351292ed8bbb3048bd89c
Ole Streicher <olebole@debian.org>
2024-01-17 09:23:07 +0100

    Replace zsvjmp.S assembler with an inline C implementation
    This makes IRAF more portable; however it also binds the use on that
    specific f2c implementation (resp. f2c.h). With this change, it will
    be impossible to use a real Fortran compiler instead of the builtin
    f2c package.
    
    The "mem" common block is also no longer defined.

--- unix/hlib/libc/iraf_setjmp.h
+++ unix/hlib/libc/iraf_setjmp.h
@@ -8,10 +8,6 @@
 #include "iraf_knames.h"
 #include "iraf_kproto.h"
 
-typedef	XINT	jmp_buf[LEN_JUMPBUF];
-static	XINT	u_jmpstat;
-
-#define	setjmp(e)	(ZSVJMP((e),&u_jmpstat),u_jmpstat)
-#define	longjmp(e,v)	(u_jmpstat=(v),ZDOJMP((e),&u_jmpstat))
+#include <setjmp.h>
 
 #endif /* D_iraf_setjmp_h */
